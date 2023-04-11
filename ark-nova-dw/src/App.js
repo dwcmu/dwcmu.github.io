f98aa969f9aea772c8760397fa40c60d0eaf5c6c
dwcmu <105466562+dwcmu@users.noreply.github.com>
2023-04-11 03:40:54 -0700

    first react app

---
+++ ark-nova-dw/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p> This is a demo ark nova app. All images and game mechanics are owned by the original game </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
